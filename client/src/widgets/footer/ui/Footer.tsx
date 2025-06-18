cdf0ceb7af885f94331db4d33e9486e52a1fbca9
d <doodleoocrypto@gmail.com>
2025-06-18 19:05:09 +0300

    Update Footer.tsx

--- client/src/widgets/footer/ui/Footer.tsx
+++ client/src/widgets/footer/ui/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>COPYRIGHT © 2024 DTEAM.TECH</p>
+            <p>COPYRIGHT © 2025 DTEAM.TECH</p>
             <div className={styles.links__container}>
                 <div className={styles.links}>
                     <h2>Celestia links</h2>
@@ -32,34 +32,31 @@ const Footer = () => {
                     <h2>TENDERMINT CHAIN SERVICES</h2>
                     <ul>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/installation-guide`}>installation guide</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/installation-guide/celestia/mainnet`}>installation guide</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/useful-commands`}>useful commands</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/snapshot/celestia/mainnet`}>snapshot</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/snapshot`}>snapshot</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/state-sync/celestia/mainnet`}>state sync</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/state-sync`}>state sync</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/endpoints/celestia/mainnet`}>endpoints</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/endpoints`}>endpoints</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/addrbook/celestia/mainnet`}>addrbook</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/addrbook`}>addrbook</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/genesis/celestia/mainnet`}>genesis</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/genesis`}>genesis</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/peers/celestia/mainnet`}>peers</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/ibc`}>IBC</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/seeds/celestia/mainnet`}>seeds</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/peers`}>peers</Link>
-                        </li>
-                        <li>
-                            <Link target="__blank" href={`https://dteam.tech/services/mainnet/seeds`}>seeds</Link>
+                            <Link target="__blank" href={`https://dteam.tech/services/useful-commands/celestia/mainnet`}>useful commands</Link>
                         </li>
                     </ul>
                 </div>
@@ -78,13 +75,13 @@ const Footer = () => {
                             <Link target="__blank" href="https://dteam.tech/">ABOUT US</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href="https://dteam.tech/features">FEATURES</Link>
+                            <Link target="__blank" href="https://dteam.tech/networks">NETWORKS</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href="https://dteam.tech/networks/mainnet">NETWORKS</Link>
+                            <Link target="__blank" href="https://dteam.tech/services">SERVICES</Link>
                         </li>
                         <li>
-                            <Link target="__blank" href="https://dteam.tech/services/mainnet">SERVICES</Link>
+                            <Link target="__blank" href="https://dteam.tech/#b2b-solutions">B2B SOLUTIONS</Link>
                         </li>
                     </ul>
                 </div>
@@ -92,7 +89,7 @@ const Footer = () => {
                 <div className={styles.links}>
                     <h2>OUR COMMUNITY</h2>
                     <ul>
-                        <li><a target="__blank" href="https://twitter.com/dteamtech">X / TWITTER</a></li>
+                        <li><a target="__blank" href="https://x.com/dteamtech">X / TWITTER</a></li>
                         <li><a target="__blank" href="https://discord.gg/aFfJH3zW4M">DISCORD</a></li>
                         <li><a target="__blank" href="https://github.com/DTEAMTECH">GITHUB</a></li>
                     </ul>
@@ -101,7 +98,7 @@ const Footer = () => {
                 <div className={styles.links}>
                     <h2>GET IN TOUCH</h2>
                     <ul>
-                        <li><a target="__blank" href="https://twitter.com/dteamtech">X / TWITTER</a></li>
+                        <li><a target="__blank" href="https://x.com/dteamtech">X / TWITTER</a></li>
                         <li><a target="__blank" href="mailto:contact@dteam.tech">EMAIL</a></li>
                     </ul>
                 </div>
